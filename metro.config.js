86783153a73338471928cc7ecf1a2b5bc079c721
tobytobyy <hongocbaolong2004@gmail.com>
2025-12-04 21:59:54 +0700

    chore(assets): add new svg icons for ui

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const { assetExts, sourceExts } = defaultConfig.resolver;
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  return mergeConfig(defaultConfig, {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  });
+})();
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -28,6 +28,16 @@ declare module '*.gif';
 declare module '*.svg';
 
 // ------------------------------------------------------
+// svg imports with react-native-svg
+// ------------------------------------------------------
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+// ------------------------------------------------------
 // JSON imports
 // ------------------------------------------------------
 declare module '*.json';
